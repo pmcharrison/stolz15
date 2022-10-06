946d58a02447a7644d5dd7823099cef8d234cba0
Brian Mulloy <brian@landlessness.net>
2022-10-06 18:47:27 -0400

    added test that stolzenburg is tonic-octave symmertical

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -8,3 +8,18 @@ test_that('changing the value of d changes the rational integer', {
   expect_equal(get_rational_interval(6,d=0.011),c(7,5))
   expect_equal(get_rational_interval(10,d=0.011),c(9,5))
 })
+test_that('inverting the intervals is symmetrical',{
+  tonic_ref = 69 # A above middle C in MIDI
+  intervals = 0:12 + tonic_ref
+  # tonic reference
+  tonic_dissonance = intervals %>% purrr::map_dbl(
+    ~smooth_log_periodicity(c(tonic_ref,.x), d=0.0102))
+
+  #octave reference
+  octave_ref = tonic_ref + 12 # A an octave above middle C in MIDI
+  octave_dissonance = intervals %>% purrr::map_dbl(
+    ~smooth_log_periodicity(c(.x,octave_ref), d=0.0102))
+
+  # expect symmetry between tonic and octave
+  expect_equal(tonic_dissonance,rev(octave_dissonance))
+})
